b864e0fc3c288fe5778f9d11fb607e0e412716fd
luvisoniasty <luvisonx@gmail.com>
2019-05-29 15:52:55 +0200

    netlify fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import SmoothScroll from 'smooth-scroll';
 import MainTemplate from 'templates/MainTemplate/MainTemplate';
 import Header from 'components/Header/Header';
 import Hero from 'components/Hero/Hero';
@@ -8,31 +7,28 @@ import AboutMe from 'components/AboutMe/AboutMe';
 import Portfolio from 'components/Portfolio/Portfolio';
 import Contact from 'components/Contact/Contact';
 import Footer from 'components/Footer/Footer';
+import SmoothScroll from 'smooth-scroll';
+
+//eslint-disable-next-line
+const scroll = new SmoothScroll('a[href*="#"]', { speed: 600 }); 
 
 const SectionWrapper = styled.div`
     overflow: hidden;
 `;
 
-class IndexPage extends React.Component {
-  componentWillMount() {
-    if (typeof window !== "undefined") {
-      SmoothScroll('a[href*="#"]', { speed: 600 });
-    }
-  }
-  render() {
-    return (
-      <MainTemplate>
-        <SectionWrapper>
-          <Header/>
-          <Hero/>
-          <AboutMe/>
-          <Portfolio/>
-          <Contact/>
-          <Footer/>
-        </SectionWrapper>
-      </MainTemplate>
-    )
-  }
-};
+const IndexPage = () => {
+  return (
+    <MainTemplate>
+      <SectionWrapper>
+        <Header/>
+        <Hero/>
+        <AboutMe/>
+        <Portfolio/>
+        <Contact/>
+        <Footer/>
+      </SectionWrapper>
+    </MainTemplate>
+  );
+}
 
 export default IndexPage
